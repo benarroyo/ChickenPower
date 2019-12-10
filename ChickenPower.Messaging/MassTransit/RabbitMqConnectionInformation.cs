ba0ee155a7f701eccbcbe6c5a179e5373334abf5
Ben Arroyo <ben.arroyo@ringleadersolutions.com>
2019-12-10 18:41:36 +0000

    Updated MassTransit version and fixed configuration and scheduling bug.

--- ChickenPower.Messaging/MassTransit/RabbitMqConnectionInformation.cs
+++ ChickenPower.Messaging/MassTransit/RabbitMqConnectionInformation.cs
@@ -2,16 +2,16 @@
 {
     public static class RabbitMqConnectionInformation
     {
-        public static string Uri { get; } = "rabbitmq://10.0.75.1:5672/SheepPower";
+        public static string Uri { get; } = "rabbitmq://rabbit-mq:5672/ChickenPower";
 
         public static string Username { get; } = "guest";
 
         public static string Password { get; } = "guest";
 
-        public static string ProposalSagaEndpoint { get; } = "rabbitmq://10.0.75.1:5672/SheepPower/proposal_saga";
+        public static string ProposalSagaEndpoint { get; } = "rabbitmq://rabbit-mq:5672/ChickenPower/proposal_saga";
 
-        public static string ContractGeneratorEndpoint { get; } = "rabbitmq://10.0.75.1:5672/SheepPower/contract_generator_data";
+        public static string ContractGeneratorEndpoint { get; } = "rabbitmq://rabbit-mq:5672/ChickenPower/contract_generator_data";
 
-        public static string PricingServiceEndpoint { get; } = "rabbitmq://10.0.75.1:5672/SheepPower/pricing_service_data";
+        public static string PricingServiceEndpoint { get; } = "rabbitmq://rabbit-mq:5672/ChickenPower/pricing_service_data";
     }
 }
